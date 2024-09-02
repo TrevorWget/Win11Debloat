88a9015856c98a5cfbe163cd783c7cfb0e535c92
TrevorWget <contact@trevorwilliams.dev>
2024-09-02 15:56:48 -0500

    Update Get.ps1

--- Get.ps1
+++ Get.ps1
@@ -57,7 +57,7 @@ Write-Output "------------------------------------------------------------------
 Write-Output "> Downloading Win11Debloat..."
 
 # Download latest version of Win11Debloat from github as zip archive
-Invoke-WebRequest http://github.com/raphire/win11debloat/archive/master.zip -OutFile "$env:TEMP/win11debloat-temp.zip"
+Invoke-WebRequest http://github.com/TrevorWget/win11debloat/archive/master.zip -OutFile "$env:TEMP/win11debloat-temp.zip"
 
 # Remove old script folder if it exists
 if (Test-Path "$env:TEMP/Win11Debloat") {
